05fe563349ad63560b18850c5cc091ad776c7e37
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-09-11 12:55:43 +0530

    Create main.dart

---
+++ OneSignal push Notification with flutter projects/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+
+  // Initialize OneSignal
+  OneSignal.initialize("eed83693-e5fd-4162-86b6-e139607e751d");
+
+  // The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission(true);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key})
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: TextButton(
+            onPressed: () async {
+              print("TextButton pressed");
+            },
+            child: Text("Send OneSignal Token to Node.js"),
+          ),
+        ),
+      ),
+    );
+  }
+}
